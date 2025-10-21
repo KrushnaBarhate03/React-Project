cb7b52ba6dc07c949cbf3923e224f4f391f02000
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-10-21 23:58:31 +0530

    Completed

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return(
     <>
     <div>
-      <h3 className='text-center font-serif font-bold text-sm sm:text-2xl text-orange-500 mt-3'>See the World from a Higher Perspective.</h3>
+      <h3 className='text-center font-serif font-bold text-lg sm:text-2xl text-orange-500 mt-3'>See the World from a Higher Perspective.</h3>
       <h4 className="text-center font-serif font-semibold text-orange-500 mt-1">We design and deliver next-generation drones built for precision, performance, and limitless possibilities.</h4>
     </div>
     <div className="flex flex-col justify-center items-center mt-5">
@@ -15,9 +15,9 @@ function Home() {
     <img src={Dchart} alt="drone chart" className=" mt-4 w-100   rounded-lg "/>
     </div>
     <div className="">
-      <h3 className="text-center font-serif font-bold text-xl w-120 mx-auto mt-6 bg-amber-500"> Our Drones</h3> 
+      <h3 className="text-center font-serif font-bold text-xl w-auto sm:w-120 mx-auto mt-6 bg-amber-500"> Our Drones</h3> 
        </div>
-       <div className="flex flex-row mt-10 flex-wrap">
+       <div className="flex flex-row sm:mt-10 m-6 flex-wrap">
         <Cards image={ Agriculture } name="Agriculture Drone"/>
          <Cards image={ Agriculture } name="Defense Drone"/>
         <Cards image={ Agriculture } name="Survelliance Drone"/> 
