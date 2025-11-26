87f44eb3141c55b15e629dc51a56a7e1b170f711
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-11-26 23:45:49 +0530

    chnagesss

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -34,12 +34,12 @@ function About() {
       <div className=" sm:flex  bg-blue-100 rounded-md">
 
         <div className="flex sm:flex-col flex-row ">
-          <button className="bg-orange-500  rounded-sm text-white flex justify-center items-center  m-10 font-serif font-bold sm:text-6xl text-2xl sm:w-24 sm:h-24 h-20 w-20 text-center " onClick={()=>setShow(1)}>1</button>
-          <button className="bg-orange-500 rounded-sm text-white m-10 flex justify-center items-center font-serif font-bold text-center h-20 w-20 sm:w-24 sm:h-24 text-2xl sm:text-6xl" onClick={()=>setShow(2)}>2</button>
-          <button className="bg-orange-500 rounded-sm text-white m-10 flex justify-center items-center font-serif font-bold text-center sm:w-24 sm:h-24 w-20 h-20 text-2xl sm:text-6xl "onClick={()=>setShow(3)}>3</button>
+          <button className="bg-orange-500  rounded-sm text-white flex justify-center items-center  m-10 font-serif font-bold sm:text-6xl text-2xl sm:w-24 sm:h-24 h-14 w-20 text-center " onClick={()=>setShow(1)}>1</button>
+          <button className="bg-orange-500 rounded-sm text-white m-10 flex justify-center items-center font-serif font-bold text-center h-14 w-20 sm:w-24 sm:h-24 text-2xl sm:text-6xl" onClick={()=>setShow(2)}>2</button>
+          <button className="bg-orange-500 rounded-sm text-white m-10 flex justify-center items-center font-serif font-bold text-center sm:w-24 sm:h-24 w-20 h-14 text-2xl sm:text-6xl "onClick={()=>setShow(3)}>3</button>
         </div>
         {Show===1 && (
-          <div>
+          <div className="flex flex-col">
             
             <h3 className="sm:m-12 m-5 font-serif text-center font-bold text-2xl bg-orange-500 p-2 rounded-sm text-white ">We Make a Different Type Of Drone Using New And Innovative Technology </h3>
             <h3 className=" font-bold font-serif mx-auto text-xl text-white text-center bg-blue-500 w-80 m-5 p-2 rounded-md">Defence Drone</h3>
@@ -55,7 +55,7 @@ function About() {
       <div>
         <h3 className="m-12 font-serif text-center font-bold text-2xl bg-red-500  p-2 rounded-sm text-white">Our Dream Is To Build The Best And Most Economical Drone In The World </h3>
         <h3 className=" font-bold font-serif mx-auto text-xl text-white text-center bg-blue-500 w-80 m-5 p-2 rounded-md ">Our Qualities</h3>
-        <div className="flex flex-wrap ">
+        <div className="flex flex-wrap justify-center ">
          <h3 className=" font-bold font-serif mx-12 text-xl text-white text-center bg-blue-500 w-80 m-5 p-2 rounded-md">Affordable Price</h3>
          <h3 className=" font-bold font-serif mx-12 text-xl text-white text-center bg-blue-500 w-80 m-5 p-2 rounded-md ">24/7 Support</h3>
          <h3 className=" font-bold font-serif mx-12 text-xl text-white text-center bg-blue-500 w-80 m-5 p-2 rounded-md">Free Maintainces For 1 Year</h3>
@@ -68,9 +68,9 @@ function About() {
       )}
 
       {Show===3 &&(
-        <div>
+        <div className='flex flex-col'>
           <h3 className="m-12 font-serif text-center font-bold text-2xl bg-white shadow-lg shadow-blue-500/50 p-2 rounded-sm text-black">“We provide training and organize awareness campaigns to educate farmers on using drones in agriculture, especially for spraying pesticides on crops.”</h3>
-           <img src={Chart} alt="Drone image " className="w-96 rounded-sm mx-auto"/> 
+           <img src={Chart} alt="Drone image " className="w-64 sm:w-96 rounded-sm mx-auto "/> 
         </div>
       )}
       </div>
