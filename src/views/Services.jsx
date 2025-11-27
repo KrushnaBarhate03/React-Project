c36042cf36c2a1b6710bae5bb5a69a1ff10765fa
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-11-27 21:31:21 +0530

    services

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>Services</div>
+     <div className='bg-blue-50 border border-blue-500 rounded-lg mt-5 p-4 w-5/6 mx-auto'>
+        <h2 className="text-center font-serif font-bold text-2xl">Our Services</h2>
+        <h3 className="text-center font-serif text-xl">“We provide a complete range of advanced drone technology solutions designed to enhance productivity, improve safety, and accelerate growth. From precision agriculture to aerial surveying, our services help businesses adopt smart and efficient drone-based operations.”</h3>
+      </div>
   )
 }
 
