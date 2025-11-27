c28b21fc253d4557564fc9d8199615afc372acbe
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-11-27 20:43:38 +0530

    changes

--- src/components/Aboutcard.jsx
+++ src/components/Aboutcard.jsx
@@ -11,7 +11,7 @@ function Aboutcard({}) {
   return (
 Data.map((card,index)=>{
     return(
-        <div className="bg-blue-100 w-auto h-64 mt-15 m-4 rounded-md border border-blue-500 ">
+        <div className="bg-blue-100 w-auto h-auto mt-15 m-4 rounded-md border border-blue-500 ">
             <img src={card.image} alt="Profile image" className="w-25 h-25 mx-auto mt-4 border-2 border-blue-500 rounded-full"/>
             <h3 className="font-serif font-bold text-xl text-center mt-2">{card.name}</h3>
             <h4 className="font-serif text-md p-6">{card.description}</h4>
