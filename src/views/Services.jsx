b7ef0d2f310a1ca5742bf8aaf5a9e94a72947152
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-11-28 16:17:16 +0530

    Services

--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -1,16 +1,52 @@
 import React from 'react'
 import Ourservices from '../components/Ourservices.jsx'
+import Section from '../components/Section.jsx'
+import Explore from '../assets/Explore.png'
+import Engineer from '../assets/Engineer.png'
+import Execute from '../assets/Execute.png'
+import Evaluate from '../assets/Evaluate.png'
+import Elevate from '../assets/Elevate.png'
 function Services() {
   return (
     <div>
-     <div className='bg-blue-50 border border-blue-500 rounded-lg mt-5 p-4 w-5/6 mx-auto'>
+      <div className='bg-blue-50 border border-blue-500 rounded-lg mt-5 p-4 w-5/6 mx-auto'>
         <h2 className="text-center font-serif font-bold text-2xl">Our Services</h2>
         <h3 className="text-center font-serif text-xl">“We provide a complete range of advanced drone technology solutions designed to enhance productivity, improve safety, and accelerate growth. From precision agriculture to aerial surveying, our services help businesses adopt smart and efficient drone-based operations.”</h3>
       </div>
       <div className="flex flex-row flex-wrap mt-15 justify-center items-center">
-    <Ourservices/>
+        <Ourservices />
       </div>
+      <Section heading="Best Process to Build Drone " />
+
+      <div className=" bg-blue-100 mt-15  p-4 flex rounded-sm  sm:flex-col flex-row border border-blue-500" >
+        <div>
+          <button type="button" className="m-4" onClick={() => console.log('Imaged clicked')}>
+            <img src={Explore} alt='Explore image' className="w-12 h-12 " /><span className="font-serif font-bold text-amber-500">Explore</span>
+          </button>
+        </div>
+        <div>
+          <button type="button" className="m-4">
+            <img src={Engineer} alt='Engineer Image' className="w-12 h-12" /><span className="font-serif font-bold text-amber-500"> Engineer</span>
+           </button>
+        </div>
+        <div>
+          <button type="button" className="m-4">
+            <img src={Execute} alt="Execute image" className='w-12 h-12' /><span className="font-serif font-bold text-amber-500">Execute</span>
+          </button>
+        </div>
+        <div>
+          <button type="button" className="m-4">
+            <img src={Evaluate} alt="Evaluate image" className="w-12 h-12" /><span className="font-serif font-bold text-amber-500">Evaluate</span>
+          </button>
+        </div>
+        <div>
+          <button type="button" className="m-4">
+            <img src={Elevate} alt='Elevate image' className="w-12 h-12" /><span className="font-serif font-bold text-amber-500">Elevate</span>
+          </button>
+        </div>
       </div>
+
+    </div>
   )
 }
 
